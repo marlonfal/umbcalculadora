65c49e62a9ea230f33dd17a8919a78dd61030aa1
marlonfal <marlonfal@hotmail.com>
2019-05-03 22:30:24 -0500

    Se añaden actividades de sumar, restar, multiplicar y dividir

---
+++ app/src/main/java/umb/calculadora/DividirActivity.java
@@ -0,0 +1,13 @@
+package umb.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DividirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dividir);
+    }
+}
--- app/src/main/java/umb/calculadora/MainActivity.java
+++ app/src/main/java/umb/calculadora/MainActivity.java
@@ -1,13 +1,74 @@
 package umb.calculadora;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSumar;
+    private Button btnRestar;
+    private Button btnMultiplicar;
+    private Button btnDividir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnSumar = findViewById(R.id.btnSumar);
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitySumar();
+            }
+        });
+
+        btnRestar = findViewById(R.id.btnRestar);
+        btnRestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityRestar();
+            }
+        });
+
+        btnMultiplicar = findViewById(R.id.btnMultiplicar);
+        btnMultiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityMultiplicar();
+            }
+        });
+
+        btnDividir = findViewById(R.id.btnDividir);
+        btnDividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityDividir();
+            }
+        });
+    }
+
+    public void openActivitySumar() {
+        Intent intent = new Intent(this, SumarActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityRestar() {
+        Intent intent = new Intent(this, RestarActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityMultiplicar() {
+        Intent intent = new Intent(this, MultiplicarActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityDividir() {
+        Intent intent = new Intent(this, DividirActivity.class);
+        startActivity(intent);
     }
 }
